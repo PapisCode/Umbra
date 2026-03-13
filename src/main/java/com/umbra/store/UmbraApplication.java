cd50821ff79b04151d378812eef4182dc04b2233
Gabriel Hernandez <gabhernandez@hotmail.ca>
2026-03-13 00:29:18 -0400

    Initialize Spring Boot project with Maven and dependencies

---
+++ src/main/java/com/umbra/store/UmbraApplication.java
@@ -0,0 +1,13 @@
+package com.umbra.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmbraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umbra/store/UmbraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umbra.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmbraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
